a723ec773d31f87d85d893e1c0eb15c153232e5f
Tharindu Gihan <Tharindu Gihan>
2017-12-30 14:38:40 +0530

    newws

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.listen(process.env.PORT || 8080);
 
 const forceSSL = function() {
   return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'http') {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
       return res.redirect(
        ['https://', req.get('Host'), req.url].join('')
       );
